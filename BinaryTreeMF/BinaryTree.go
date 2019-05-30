74ea0ff1b2c42b56b6d2d2c6d990677b26304bca
PranavBakre <psbakre123@gmail.com>
2019-05-30 14:36:08 +0530

    formatted

--- BinaryTreeMF/BinaryTree.go
+++ BinaryTreeMF/BinaryTree.go
@@ -28,19 +28,22 @@ func main() {
 	node, _ := bst.Search(key)
 	if node != nil {
 		fmt.Println(node.Data)
+	}else {
+		fmt.Println(key," not found\n")
 	}
 	bst.Delete()
+	fmt.Println("\nInorder display")
 	bst.InNRdisplay()
-	fmt.Println()
+	fmt.Println("\nBFS display\n")
 	bst.BFS()
 
 	var bst2 BinaryTree.BST
 	bst2.CopyTreeNR(&bst)
 	fmt.Println()
 	fmt.Println()
-	fmt.Println()
+	fmt.Println("Displaying Copied Tree")
 	bst2.InNRdisplay()
-	fmt.Println()
+	fmt.Println("\nMirror\nInorder display")
 	bst2.MirrorNR()
 	bst2.InNRdisplay()
 	fmt.Println()
