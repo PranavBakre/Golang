b8f1db96ceebb1dd639e5c4a11387fb8a55a4d92
PranavBakre <psbakre123@gmail.com>
2019-08-31 23:26:28 +0530

    Cleaned Graph struct and shifted data to node

--- Distance Vector-Go/Graph/Graph.go
+++ Distance Vector-Go/Graph/Graph.go
@@ -6,17 +6,24 @@ import (
 )
 
 type Node struct { //Contents of every node
+	//Current Data
 	Distance  []int
 	Next      []int
 	Neighbour []int
+
+	//Update Data
+	UpdateDistance  []int
+	UpdateNext      []int
+	IsUpdatePresent bool
+	//Initial Data
+	InitDistance  []int
+	InitNext      []int
+	InitNeighbour []int
 }
 
 type Graph struct { //Network Map
-	NodeNumber      int
-	Graph           []Node
-	Update          []Node
-	InitData        []Node
-	IsUpdatePresent []bool
+	NodeNumber int
+	Graph      []Node
 }
 
 func (G *Graph) Init() { //Initialises the network map
@@ -25,27 +32,28 @@ func (G *Graph) Init() { //Initialises the network map
 	fmt.Println(G.NodeNumber)
 
 	G.Graph = make([]Node, G.NodeNumber)
-	G.InitData = make([]Node, G.NodeNumber)
-	G.Update = make([]Node, G.NodeNumber)
-	G.IsUpdatePresent = make([]bool, G.NodeNumber)
-	//G.Neighbour = make([][]int, G.NodeNumber)
 	for i := 0; i < G.NodeNumber; i++ {
+		//Node Current Data Allocation
 		G.Graph[i].Distance = make([]int, G.NodeNumber)
-		G.Update[i].Distance = make([]int, G.NodeNumber)
 		G.Graph[i].Neighbour = make([]int, G.NodeNumber)
-		//	G.Neighbour[i] = make([]int, G.NodeNumber)
-		G.InitData[i].Distance = make([]int, G.NodeNumber)
-		G.InitData[i].Neighbour = make([]int, G.NodeNumber)
 		G.Graph[i].Next = make([]int, G.NodeNumber)
-		G.Update[i].Next = make([]int, G.NodeNumber)
-		G.InitData[i].Next = make([]int, G.NodeNumber)
+		//Update Data Allocation
+		G.Graph[i].UpdateDistance = make([]int, G.NodeNumber)
+		G.Graph[i].UpdateNext = make([]int, G.NodeNumber)
+
+		//Init Data allocation
+		G.Graph[i].InitDistance = make([]int, G.NodeNumber)
+		G.Graph[i].InitNeighbour = make([]int, G.NodeNumber)
+		G.Graph[i].InitNext = make([]int, G.NodeNumber)
+
 	}
 	for i := 0; i < G.NodeNumber; i++ {
 		for j := 0; j < G.NodeNumber; j++ {
 			G.Graph[i].Distance[j] = -1
 			G.Graph[i].Next[j] = -1
-			G.Update[i].Distance[j] = 999
 			G.Graph[i].Neighbour[j] = 0
+			G.Graph[i].UpdateDistance[j] = 999
+			G.Graph[i].UpdateNext[j] = -1
 		}
 	}
 
@@ -74,9 +82,9 @@ func (G *Graph) AddData() { //Adding the data into the map
 					G.Graph[j].Next[i] = i
 				}
 			}
-			G.InitData[i].Distance[j] = G.Graph[i].Distance[j]
-			G.InitData[i].Next[j] = G.Graph[i].Next[j]
-			G.InitData[i].Neighbour[j] = G.Graph[i].Neighbour[j]
+			G.Graph[i].InitDistance[j] = G.Graph[i].Distance[j]
+			G.Graph[i].InitNext[j] = G.Graph[i].Next[j]
+			G.Graph[i].InitNeighbour[j] = G.Graph[i].Neighbour[j]
 		}
 	}
 	fmt.Println("Initial Data")
@@ -84,31 +92,36 @@ func (G *Graph) AddData() { //Adding the data into the map
 }
 
 func (G *Graph) Display() { //Display the node data
+	fmt.Println("Node No. Distance Vector\tNext Hop\t Neighbour")
 	for i := 0; i < G.NodeNumber; i++ {
 
-		fmt.Printf("Node %d:%+v\n", i, G.Graph[i])
+		fmt.Printf("%d \t%+v\t\t%+v\t%+v\n", i, G.Graph[i].Distance, G.Graph[i].Next, G.Graph[i].Next)
 	}
 	fmt.Println("*************************************************\n1.Neighbour indicated by 1.Node Number=Position\nNext Hop:=-1 implies no next hop.i.e. Node itself\n*************************************************")
 }
 
 func (G *Graph) UpdateData(Node int) { //Function to update the distance vector at every node
 	for i := 0; i < G.NodeNumber; i++ {
-		if i != Node && G.Graph[Node].Distance[i] > G.Update[Node].Distance[i] {
-			G.Graph[Node].Distance[i] = G.Update[Node].Distance[i]
-			G.Graph[Node].Next[i] = G.Update[Node].Next[i]
+		if i != Node && G.Graph[Node].Distance[i] > G.Graph[Node].UpdateDistance[i] { //if i != Node && G.Graph[Node].Distance[i] > G.Update[Node].Distance[i] {
+			G.Graph[Node].Distance[i] = G.Graph[Node].UpdateDistance[i] //G.Update[Node].Distance[i]
+			G.Graph[Node].Next[i] = G.Graph[Node].UpdateNext[i]         //G.Update[Node].Next[i]
 		}
 	}
-	G.IsUpdatePresent[Node] = false
+	//G.IsUpdatePresent[Node] = false
+	G.Graph[Node].IsUpdatePresent = false
 }
 
 func (G *Graph) SendData(wg *sync.WaitGroup, Sender int, Node int) { //Concurrently send the data to all neighbours
 	for i := 0; i < G.NodeNumber; i++ {
 		if i != Node {
 			v := G.Graph[Sender].Distance[i] + G.Graph[Sender].Distance[Node]
-			if v < G.Update[Node].Distance[i] {
-				G.Update[Node].Distance[i] = v
-				G.Update[Node].Next[i] = Sender
-				G.IsUpdatePresent[Node] = true
+			if v < G.Graph[Node].UpdateDistance[i] { //G.Update[Node].Distance[i] {
+				//G.Update[Node].Distance[i] = v
+				G.Graph[Node].UpdateDistance[i] = v
+				//G.Update[Node].Next[i] = Sender
+				G.Graph[Node].UpdateNext[i] = Sender
+				//G.IsUpdatePresent[Node] = true
+				G.Graph[Node].IsUpdatePresent = true
 			}
 		}
 	}
@@ -127,7 +140,7 @@ func (G *Graph) Run() { //Emulate the running of distance vector
 	}
 	wg.Wait()
 	for i := 0; i < G.NodeNumber; i = (i + 1) % G.NodeNumber {
-		if G.IsUpdatePresent[i] {
+		if G.Graph[i].IsUpdatePresent { //G.IsUpdatePresent[i] {
 			G.UpdateData(i)
 
 			for j := 0; j < G.NodeNumber; j++ {
@@ -140,7 +153,7 @@ func (G *Graph) Run() { //Emulate the running of distance vector
 			wg.Wait()
 		}
 
-		for k = 0; k < G.NodeNumber && G.IsUpdatePresent[k] == false; k++ {
+		for k = 0; k < G.NodeNumber && G.Graph[k].IsUpdatePresent == false; k++ {
 		}
 		if k == G.NodeNumber && i == G.NodeNumber-1 {
 			break
@@ -155,7 +168,11 @@ func (G *Graph) ChangeData() { //Change the data of the nodes to indicate broken
 	var i, j, c int
 	fmt.Println("Enter the nodes and new cost")
 	fmt.Scanln(&i, &j, &c)
-	G.Graph = G.InitData
+	for i := 0; i < G.NodeNumber; i++ {
+		G.Graph[i].Distance = G.Graph[i].InitDistance
+		G.Graph[i].Next = G.Graph[i].InitNext
+		G.Graph[i].Neighbour = G.Graph[i].InitNeighbour
+	}
 
 	G.Graph[i].Distance[j] = c
 	G.Graph[j].Distance[i] = c
@@ -165,7 +182,7 @@ func (G *Graph) ChangeData() { //Change the data of the nodes to indicate broken
 	}
 	for ii := 0; ii < G.NodeNumber; ii++ {
 		for jj := 0; jj < G.NodeNumber; jj++ {
-			G.Update[ii].Distance[jj] = 999
+			G.Graph[ii].UpdateDistance[jj] = 999 //G.Update[ii].Distance[jj] = 999
 		}
 	}
 }
